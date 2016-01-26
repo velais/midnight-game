908f40c9c7ce37694112ebe908d667fb627eeaaf
velais <velais@live.com>
2016-01-26 21:40:25 +1300

    Use sdl2. Make event loop

--- midnight/src/main.rs
+++ midnight/src/main.rs
@@ -1,19 +1,38 @@
-extern crate piston_window;
+extern crate piston;
+extern crate sdl2;
+extern crate sdl2_window;
+extern crate fps_counter;
 
-use piston_window::*;
+use sdl2_window::Sdl2Window;
+use piston::window::{ Size, Window, AdvancedWindow, OpenGLWindow, WindowSettings };
+use piston::event_loop::{ Events, EventLoop };
 
-fn main() {
-    println!("Hello, Midnight!");
+pub fn main() {
 
-    let window: PistonWindow = 
-        WindowSettings::new("Hello Midnight!", [640, 480])
-        .exit_on_esc(true).build().unwrap();
-    for e in window {
-        e.draw_2d(|c, g| {
-            clear([1.0; 4], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], 
-                      [0.0, 0.0, 100.0, 100.0],
-                      c.transform, g);
-        });
+    let mut fps_counter = fps_counter::FPSCounter::new();
+
+    let mut window: Sdl2Window = WindowSettings::new(
+        "Hello Piston!", 
+        Size { width: 640, height: 480 })
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut events = window.events().ups(60).max_fps(100);
+    while let Some(e) = events.next(&mut window) {
+        use piston::input::Event;
+
+        match e {
+            Event::Render(_) => {
+                let fps = fps_counter.tick();
+                let title = format!(
+                    "Midnight FPS: {}",
+                    fps);
+                window.set_title(title);
+            }
+            Event::AfterRender(_) => {}
+            Event::Update(_) => {}
+            _ => {}
+        }
     }
 }
